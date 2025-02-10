5413420ac70f00e56cdcaf3626ef85ce515d7dff
1minepowminx <nobusymaile@gmail.com>
2025-02-10 21:02:07 +0200

    Specified import of windll

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sys
-import ctypes
+from ctypes import windll
 from tray.tray_icon import create_tray_icon
 from config.config import is_first_run, set_first_run_complete
 from messages.first_run import show_first_run_message
@@ -8,8 +8,8 @@ from messages.first_run import show_first_run_message
 def main():
     """Основная функция программы."""
     mutex_name = "SysInfoMutex"
-    handle = ctypes.windll.kernel32.CreateMutexW(None, False, mutex_name)
-    last_error = ctypes.windll.kernel32.GetLastError()
+    handle = windll.kernel32.CreateMutexW(None, False, mutex_name)
+    last_error = windll.kernel32.GetLastError()
 
     if last_error == 183:  # ERROR_ALREADY_EXISTS
         sys.exit()
@@ -21,7 +21,7 @@ def main():
     icon = create_tray_icon()
     icon.run()
 
-    ctypes.windll.kernel32.ReleaseMutex(handle)
+    windll.kernel32.ReleaseMutex(handle)
 
 
 if __name__ == "__main__":
