a259513b3e8d3b9c0586ec6330f88cb8433e9ffd
1minepowminx <nobusymaile@gmail.com>
2025-10-10 11:47:26 +0200

    Add mutex system to prevent the application copy launching

--- main.cpp
+++ main.cpp
@@ -3,10 +3,33 @@
 #include <QApplication>
 #include <QLocale>
 #include <QTranslator>
+#include <QSystemSemaphore>
+#include <QSharedMemory>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    const QString sharedKey = "SysInfoMutex"; // For correct operation, the name must match version 1
+
+    // A semaphore is needed to avoid race conditions when multiple processes start simultaneously
+    QSystemSemaphore semaphore(sharedKey + "_sem", 1);
+    semaphore.acquire();
+
+    QSharedMemory sharedMemory(sharedKey);
+    bool isAlreadyRunning = false;
+
+    if (!sharedMemory.create(1)) {
+        // If the memory already exists, it means that the program has already been launched
+        isAlreadyRunning = true;
+    }
+
+    semaphore.release();
+
+    if (isAlreadyRunning) {
+        // Just finish the application if there is already an instance
+        return 0;
+    }
 
     QTranslator translator;
     const QStringList uiLanguages = QLocale::system().uiLanguages();
