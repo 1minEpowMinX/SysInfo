fee84c52f3f45f82f1de7bcc123c8242c236b81e
1minepowminx <nobusymaile@gmail.com>
2025-10-14 11:34:35 +0200

    Update localization files

--- resources/i18n/SysInfo_en_US.ts
+++ resources/i18n/SysInfo_en_US.ts
@@ -2,19 +2,38 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="en_US">
 <context>
+    <name>App</name>
+    <message>
+        <location filename="../../app/app.cpp" line="104"/>
+        <source>Справка</source>
+        <translation>Help</translation>
+    </message>
+</context>
+<context>
     <name>QObject</name>
     <message>
-        <location filename="../app/app.cpp" line="32"/>
+        <location filename="../../app/app.cpp" line="71"/>
         <source>Ошибка</source>
         <translation>Error</translation>
     </message>
     <message>
-        <location filename="../app/app.cpp" line="32"/>
+        <location filename="../../app/app.cpp" line="71"/>
         <source>Системный трей недоступен.</source>
         <translation>System tray is not available.</translation>
     </message>
     <message>
-        <location filename=".../utils/utils.cpp" line="59"/>
+        <location filename="../../utils/utils.cpp" line="73"/>
+        <location filename="../../utils/utils.cpp" line="84"/>
+        <source>Недоступно</source>
+        <translation>Unavailable</translation>
+    </message>
+    <message>
+        <location filename="../../utils/utils.cpp" line="87"/>
+        <source>Не поддерживается</source>
+        <translation>Not supported</translation>
+    </message>
+    <message>
+        <location filename="../../utils/utils.cpp" line="98"/>
         <source>Имя устройства: %1
 Пользователь: %2
 IP-адрес: %3
@@ -25,37 +44,115 @@ IP address: %3
 Uptime: %4</translation>
     </message>
     <message>
-        <location filename="../app/app.cpp" line="69"/>
+        <location filename="../../app/app.cpp" line="44"/>
+        <source>SysInfo работает в фоне</source>
+        <translation>SysInfo is running in the background</translation>
+    </message>
+    <message>
+        <location filename="../../app/app.cpp" line="45"/>
+        <source>Приложение собирает системную информацию и помогает в диагностике.
+Подробнее см. раздел &quot;Справка&quot;.</source>
+        <translation>The application collects system information and helps with diagnostics.
+See the "Help" section for more details.</translation>
+    </message>
+    <message>
+        <location filename="../../app/app.cpp" line="57"/>
+        <source>Сделайте значок видимым в трее</source>
+        <translation>Make the icon visible in the tray</translation>
+    </message>
+    <message>
+        <location filename="../../app/app.cpp" line="58"/>
+        <source>Перетащите значок SysInfo в область уведомлений.
+Нажмите сюда, чтобы открыть подробную инструкцию.</source>
+        <translation>Drag the SysInfo icon to the notification area.
+Click here to open the detailed guide.</translation>
+    </message>
+    <message>
+        <location filename="../../app/app.cpp" line="103"/>
         <source>Скопировать в буфер обмена</source>
         <translation>Copy to clipboard</translation>
     </message>
     <message>
-        <location filename="../app/app.cpp" line="70"/>
-        <location filename="../app/app.cpp" line="87"/>
+        <location filename="../../app/app.cpp" line="105"/>
+        <location filename="../../tray/aboutdialog.cpp" line="16"/>
+        <source>О программе</source>
+        <translation>About</translation>
+    </message>
+    <message>
+        <location filename="../../app/app.cpp" line="106"/>
+        <location filename="../../app/app.cpp" line="151"/>
         <source>Выход</source>
         <translation>Exit</translation>
     </message>
     <message>
-        <location filename="../app/app.cpp" line="83"/>
+        <location filename="../../app/app.cpp" line="126"/>
         <source>Системная информация</source>
         <translation>System information</translation>
     </message>
     <message>
-        <location filename="../app/app.cpp" line="83"/>
+        <location filename="../../app/app.cpp" line="126"/>
         <source>Информация скопирована в буфер обмена.</source>
         <translation>Information copied to clipboard.</translation>
     </message>
     <message>
-        <location filename="../app/app.cpp" line="88"/>
-        <source>Мониторинг собирает данные, которые помогут в устранении неполадок.
+        <location filename="../../app/app.cpp" line="130"/>
+        <source>&lt;b&gt;Приложение собирает системную информацию и помогает в диагностике:&lt;/b&gt;&lt;br&gt;• Отображает сведения о устройстве (имя, пользователь, IP, время работы).&lt;br&gt;• Работает в фоновом режиме через системный трей.&lt;br&gt;• Нажмите правой кнопкой на значке, чтобы открыть меню действий.&lt;br&gt;• Пункт «Скопировать в буфер» копирует текущие сведения.&lt;br&gt;&lt;br&gt;Если значок в трее не виден – перетащите его в область уведомлений.</source>
+        <translation>&lt;b&gt;The application collects system information and helps with diagnostics:&lt;/b&gt;&lt;br&gt;• Shows device details (name, user, IP, uptime).&lt;br&gt;• Runs in the background via the system tray.&lt;br&gt;• Right-click the icon to open the action menu.&lt;br&gt;• The "Copy to clipboard" item copies the current information.&lt;br&gt;&lt;br&gt;If the tray icon is not visible, drag it into the notification area.</translation>
+    </message>
+    <message>
+        <location filename="../../app/app.cpp" line="139"/>
+        <source>Справка</source>
+        <translation>Help</translation>
+    </message>
+    <message>
+        <location filename="../../app/app.cpp" line="148"/>
+        <source>Приложение собирает системную информацию и помогает в диагностике.
 Всё равно закрыть приложение?</source>
-        <translation>Monitoring is collecting data that will help troubleshoot issues.
+        <translation>The application collects system information and helps with diagnostics.
 Close the application anyway?</translation>
     </message>
     <message>
-        <location filename="../utils/utils.cpp" line="43"/>
+        <location filename="../../utils/utils.cpp" line="58"/>
         <source>Нет IP</source>
         <translation>No IP</translation>
+    </message>
+    <message>
+        <location filename="../../tray/aboutdialog.cpp" line="32"/>
+        <source>&lt;h2&gt;&lt;b&gt;SysInfo&lt;/b&gt;&lt;/h2&gt;&lt;p&gt;&lt;b&gt;Версия:&lt;/b&gt; %1 (build %2)&lt;/p&gt;&lt;p&gt;&lt;b&gt;Разработчик:&lt;/b&gt; Kirill Bitskyi &lt;/p&gt;&lt;p&gt;&lt;b&gt;Компания:&lt;/b&gt; Pivdenny &lt;/p&gt;&lt;p&gt;&lt;b&gt;Исходный код:&lt;/b&gt; &lt;a href=&apos;https://github.com/1minEpowMinX/SysInfo&apos;&gt;GitHub&lt;/a&gt;&lt;/p&gt;&lt;p&gt;&lt;b&gt;Ядро:&lt;/b&gt; Qt %3, C++17&lt;/p&gt;&lt;p&gt;&lt;b&gt;Лицензия:&lt;/b&gt; &lt;a href=&apos;https://github.com/1minEpowMinX/SysInfo/blob/main/LICENSE&apos;&gt;LGPL v3.0&lt;/a&gt;&lt;/p&gt;&lt;hr&gt;&lt;p&gt;&lt;small&gt;© 2025 Kirill Bitskyi for Pivdenny. Все права защищены.&lt;/small&gt;&lt;/p&gt;</source>
+        <translation>&lt;h2&gt;&lt;b&gt;SysInfo&lt;/b&gt;&lt;/h2&gt;&lt;p&gt;&lt;b&gt;Version:&lt;/b&gt; %1 (build %2)&lt;/p&gt;&lt;p&gt;&lt;b&gt;Developer:&lt;/b&gt; Kirill Bitskyi &lt;/p&gt;&lt;p&gt;&lt;b&gt;Company:&lt;/b&gt; Pivdenny &lt;/p&gt;&lt;p&gt;&lt;b&gt;Source code:&lt;/b&gt; &lt;a href=&apos;https://github.com/1minEpowMinX/SysInfo&apos;&gt;GitHub&lt;/a&gt;&lt;/p&gt;&lt;p&gt;&lt;b&gt;Core:&lt;/b&gt; Qt %3, C++17&lt;/p&gt;&lt;p&gt;&lt;b&gt;License:&lt;/b&gt; &lt;a href=&apos;https://github.com/1minEpowMinX/SysInfo/blob/main/LICENSE&apos;&gt;LGPL v3.0&lt;/a&gt;&lt;/p&gt;&lt;hr&gt;&lt;p&gt;&lt;small&gt;© 2025 Kirill Bitskyi for Pivdenny. All rights reserved.&lt;/small&gt;&lt;/p&gt;</translation>
+    </message>
+    <message>
+        <location filename="../../tray/aboutdialog.cpp" line="73"/>
+        <source>&lt;p span style=&apos;color: gray;&apos;&gt;&lt;b&gt;ОС:&lt;/b&gt; %1&lt;br&gt;&lt;b&gt;Пользователь:&lt;/b&gt; %2&lt;br&gt;&lt;b&gt;Устройство:&lt;/b&gt; %3&lt;br&gt;&lt;b&gt;Файл настроек:&lt;/b&gt; %4&lt;/p&gt;</source>
+        <translation>&lt;p span style=&apos;color: gray;&apos;&gt;&lt;b&gt;OS:&lt;/b&gt; %1&lt;br&gt;&lt;b&gt;User:&lt;/b&gt; %2&lt;br&gt;&lt;b&gt;Device:&lt;/b&gt; %3&lt;br&gt;&lt;b&gt;Settings file:&lt;/b&gt; %4&lt;/p&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>TrayGuide</name>
+    <message>
+        <location filename="../../tray/trayguide.cpp" line="10"/>
+        <source>Как закрепить значок в трее</source>
+        <translation>How to pin the icon in the tray</translation>
+    </message>
+    <message>
+        <location filename="../../tray/trayguide.cpp" line="21"/>
+        <source>Чтобы приложение всегда было видно в области уведомлений:
+1. Откройте скрытые значки, нажав стрелку вверх рядом с системным треем.
+2️. Найдите значок SysInfo и перетащите его в видимую область панели.
+</source>
+        <translation>To keep the application visible in the notification area:
+1. Open hidden icons by clicking the up arrow next to the system tray.
+2. Find the SysInfo icon and drag it into the visible area of the taskbar.</translation>
+    </message>
+    <message>
+        <location filename="../../tray/trayguide.cpp" line="29"/>
+        <source>Больше не показывать</source>
+        <translation>Don't show again</translation>
+    </message>
+    <message>
+        <location filename="../../tray/trayguide.cpp" line="30"/>
+        <source>Закрыть</source>
+        <translation>Close</translation>
     </message>
 </context>
 </TS>
--- resources/i18n/SysInfo_ru_RU.ts
+++ resources/i18n/SysInfo_ru_RU.ts
@@ -2,55 +2,148 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="ru_RU">
 <context>
+    <name>App</name>
+    <message>
+        <location filename="../../app/app.cpp" line="104"/>
+        <source>Справка</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
     <name>QObject</name>
     <message>
-        <location filename="../app/app.cpp" line="32"/>
+        <location filename="../../app/app.cpp" line="71"/>
         <source>Ошибка</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../app/app.cpp" line="32"/>
-        <source>Системный трей недоступен!</source>
+        <location filename="../../app/app.cpp" line="44"/>
+        <source>SysInfo работает в фоне</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../app/app.cpp" line="44"/>
-        <source>Имя компьютера: %1
+        <location filename="../../app/app.cpp" line="45"/>
+        <source>Приложение собирает системную информацию и помогает в диагностике.
+Подробнее см. раздел &quot;Справка&quot;.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../app/app.cpp" line="57"/>
+        <source>Сделайте значок видимым в трее</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../app/app.cpp" line="58"/>
+        <source>Перетащите значок SysInfo в область уведомлений.
+Нажмите сюда, чтобы открыть подробную инструкцию.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../app/app.cpp" line="71"/>
+        <source>Системный трей недоступен.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../app/app.cpp" line="103"/>
+        <source>Скопировать в буфер обмена</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../app/app.cpp" line="105"/>
+        <location filename="../../tray/aboutdialog.cpp" line="16"/>
+        <source>О программе</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../app/app.cpp" line="106"/>
+        <location filename="../../app/app.cpp" line="151"/>
+        <source>Выход</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../app/app.cpp" line="126"/>
+        <source>Системная информация</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../app/app.cpp" line="126"/>
+        <source>Информация скопирована в буфер обмена.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../app/app.cpp" line="130"/>
+        <source>&lt;b&gt;Приложение собирает системную информацию и помогает в диагностике:&lt;/b&gt;&lt;br&gt;• Отображает сведения о устройстве (имя, пользователь, IP, время работы).&lt;br&gt;• Работает в фоновом режиме через системный трей.&lt;br&gt;• Нажмите правой кнопкой на значке, чтобы открыть меню действий.&lt;br&gt;• Пункт «Скопировать в буфер» копирует текущие сведения.&lt;br&gt;&lt;br&gt;Если значок в трее не виден – перетащите его в область уведомлений.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../app/app.cpp" line="139"/>
+        <source>Справка</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../app/app.cpp" line="148"/>
+        <source>Приложение собирает системную информацию и помогает в диагностике.
+Всё равно закрыть приложение?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../utils/utils.cpp" line="58"/>
+        <source>Нет IP</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../utils/utils.cpp" line="73"/>
+        <location filename="../../utils/utils.cpp" line="84"/>
+        <source>Недоступно</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../utils/utils.cpp" line="87"/>
+        <source>Не поддерживается</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../utils/utils.cpp" line="98"/>
+        <source>Имя устройства: %1
 Пользователь: %2
 IP-адрес: %3
 Время включения: %4</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../app/app.cpp" line="69"/>
-        <source>Скопировать в буфер обмена</source>
+        <location filename="../../tray/aboutdialog.cpp" line="32"/>
+        <source>&lt;h2&gt;&lt;b&gt;SysInfo&lt;/b&gt;&lt;/h2&gt;&lt;p&gt;&lt;b&gt;Версия:&lt;/b&gt; %1 (build %2)&lt;/p&gt;&lt;p&gt;&lt;b&gt;Разработчик:&lt;/b&gt; Kirill Bitskyi &lt;/p&gt;&lt;p&gt;&lt;b&gt;Компания:&lt;/b&gt; Pivdenny &lt;/p&gt;&lt;p&gt;&lt;b&gt;Исходный код:&lt;/b&gt; &lt;a href=&apos;https://github.com/1minEpowMinX/SysInfo&apos;&gt;GitHub&lt;/a&gt;&lt;/p&gt;&lt;p&gt;&lt;b&gt;Ядро:&lt;/b&gt; Qt %3, C++17&lt;/p&gt;&lt;p&gt;&lt;b&gt;Лицензия:&lt;/b&gt; &lt;a href=&apos;https://github.com/1minEpowMinX/SysInfo/blob/main/LICENSE&apos;&gt;LGPL v3.0&lt;/a&gt;&lt;/p&gt;&lt;hr&gt;&lt;p&gt;&lt;small&gt;© 2025 Kirill Bitskyi for Pivdenny. Все права защищены.&lt;/small&gt;&lt;/p&gt;</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../app/app.cpp" line="70"/>
-        <location filename="../app/app.cpp" line="87"/>
-        <source>Выход</source>
+        <location filename="../../tray/aboutdialog.cpp" line="73"/>
+        <source>&lt;p span style=&apos;color: gray;&apos;&gt;&lt;b&gt;ОС:&lt;/b&gt; %1&lt;br&gt;&lt;b&gt;Пользователь:&lt;/b&gt; %2&lt;br&gt;&lt;b&gt;Устройство:&lt;/b&gt; %3&lt;br&gt;&lt;b&gt;Файл настроек:&lt;/b&gt; %4&lt;/p&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>TrayGuide</name>
+    <message>
+        <location filename="../../tray/trayguide.cpp" line="10"/>
+        <source>Как закрепить значок в трее</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../app/app.cpp" line="83"/>
-        <source>Системная информация</source>
+        <location filename="../../tray/trayguide.cpp" line="21"/>
+        <source>Чтобы приложение всегда было видно в области уведомлений:
+1. Откройте скрытые значки, нажав стрелку вверх рядом с системным треем.
+2️. Найдите значок SysInfo и перетащите его в видимую область панели.
+</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../app/app.cpp" line="83"/>
-        <source>Информация скопирована в буфер обмена.</source>
+        <location filename="../../tray/trayguide.cpp" line="29"/>
+        <source>Больше не показывать</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../app/app.cpp" line="88"/>
-        <source>Мониторинг собирает данные, которые помогут в устранении неполадок.
-Всё равно закрыть приложение?</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../utils/utils.cpp" line="43"/>
-        <source>Нет IP</source>
+        <location filename="../../tray/trayguide.cpp" line="30"/>
+        <source>Закрыть</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
