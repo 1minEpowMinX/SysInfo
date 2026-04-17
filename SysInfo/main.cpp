7da9be243c817d3c05c8461bd6761b2d8552b27e
1minepowminx <nobusymaile@gmail.com>
2026-04-17 11:43:39 +0200

    refactor: improve process instance check and enhance translation loading logic

--- SysInfo/main.cpp
+++ SysInfo/main.cpp
@@ -2,61 +2,65 @@
 #include "logger/logger.h"
 
 #include <QApplication>
+#include <QFile>
 #include <QLocale>
 #include <QTranslator>
 #include <QSystemSemaphore>
 #include <QSharedMemory>
 
-int main(int argc, char *argv[])
+bool isAlreadyRunning()
 {
-    QApplication a(argc, argv);
-
-    const QString sharedKey = "SysInfoMutex"; // For correct operation, the name must match version 1
-
+    const QString sharedKey = "SysInfoMutex";
     // A semaphore is needed to avoid race conditions when multiple processes start simultaneously
     QSystemSemaphore semaphore(sharedKey + "_sem", 1);
     semaphore.acquire();
 
-    QSharedMemory sharedMemory(sharedKey);
-    bool isAlreadyRunning = false;
-
-    if (!sharedMemory.create(1)) {
-        // If the memory already exists, it means that the program has already been launched
-        isAlreadyRunning = true;
-    }
-
+    static QSharedMemory sharedMemory(sharedKey);
+    const bool alreadyRunning = !sharedMemory.create(1);
     semaphore.release();
 
-    if (isAlreadyRunning) {
-        // Just finish the application if there is already an instance
+    return alreadyRunning;
+}
+
+void loadTranslator(QApplication &a, QTranslator &translator)
+{
+    const QStringList uiLanguages = QLocale::system().uiLanguages();
+    QString triedLocale;
+
+    for (const QString &locale : uiLanguages) {
+        const QString baseName = "sysinfo_" + QLocale(locale).name();
+        const QString path = ":resources/i18n/" + baseName + ".qm";
+
+        if (!QFile::exists(path)) {
+            continue;
+        }
+
+        triedLocale = baseName;
+        if (translator.load(path)) {
+            a.installTranslator(&translator);
+            return;
+        }
+
+        Logger::log(Logger::EventId::TSLoadFailed,
+                    QString("Translation file exists but failed to load: %1").arg(path));
+    }
+}
+
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    if (isAlreadyRunning()) {
         return 0;
     }
 
-    bool uiLanguageLoaded = false;
-    const QString baseName;
-
     QTranslator translator;
-    const QStringList uiLanguages = QLocale::system().uiLanguages();
-    for (const QString &locale : uiLanguages) {
-        const QString baseName = "sysinfo_" + QLocale(locale).name();
-        if (translator.load(":resources/i18n/" + baseName)) {
-            a.installTranslator(&translator);
-            uiLanguageLoaded = true;
-            break;
-        }
-    }
-
-    if (!uiLanguageLoaded) {
-        Logger::log(Logger::EventId::TSLoadFailed,
-                    QString("Failed to translation. Tried: %1. Use default language.")
-                        .arg(baseName));
-
-    }
+    loadTranslator(a, translator);
 
     App::instance().startApp();
-
-    Logger::log(Logger::EventId::AppStart, QString("SysInfo started. Version=%1")
-                                               .arg(PROJECT_VERSION));
+    Logger::log(Logger::EventId::AppStart,
+                QString("SysInfo started. Version=%1").arg(PROJECT_VERSION));
 
     return a.exec();
 }
